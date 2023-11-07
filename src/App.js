fc0825900efcfa428ad14cddce88ea7544ff7ee2
viraoka <verakazyun@gmail.com>
2023-11-07 11:55:17 +0200

    modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Dishes from './Components/DishesComponenets/Dishes';
+import AllCategories from './Components/Filter/AllCategories';
+import Cart from './Components/Cart/Cart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='block'>
+      <AllCategories/>
+      <Cart/>
+      </div>
+      <div className='block'>
+      <Dishes/>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
